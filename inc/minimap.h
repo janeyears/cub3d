7c5013d8ca985cc02d373d4cf850c61175850d0b
Mariia Zhytnikova <allusio@gmail.com>
2025-05-30 16:48:30 +0300

    minimap 0 border fix

--- inc/minimap.h
+++ inc/minimap.h
@@ -3,7 +3,7 @@
 
 # include "cub3d.h"
 
-#define MINIMAP_SCALE 40           // pixels per tile
+#define MINIMAP_SCALE 30           // pixels per tile
 #define MINIMAP_RADIUS 128         // radius in pixels
 #define MINIMAP_SIZE (MINIMAP_RADIUS * 2)
 
